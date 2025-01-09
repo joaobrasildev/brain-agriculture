5b2a3d97e172c91e76268b43e36a9fb716fb8e48
João Brasil <jvobsystems@gmail.com>
2025-01-09 11:05:05 -0300

    fix: ajustando validação do zod

--- src/shared/module/config/config.schema.ts
+++ src/shared/module/config/config.schema.ts
@@ -2,13 +2,15 @@ import { z } from 'zod';
 
 export const environmentSchema = z.enum(['test', 'development', 'production']);
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const databaseSchema = z.object({
   host: z.string(),
   database: z.string(),
   password: z.string(),
   port: z.coerce.number(),
   url: z.string(),
-  username: z.string(),
+  username: isProduction ? z.string().optional() : z.string(),
 });
 
 export const configSchema = z.object({
